236d1cf0879f363c44690b094aa7760ab4fd1bbf
Liz <lizlie.holleza@awsys-i.com>
2018-08-16 18:13:12 +0800

    set adapter and itemclick listener

--- app/src/main/java/com/example/lizlieholleza/booksearch/BookActivity.java
+++ app/src/main/java/com/example/lizlieholleza/booksearch/BookActivity.java
@@ -2,20 +2,27 @@ package com.example.lizlieholleza.booksearch;
 
 import android.app.LoaderManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.Loader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>>{
     private static final int LOADER_ID = 1;
-    private TextView empty_view;
+    private TextView emptyStateView;
     private View loadingIndicator;
+    private BookAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +32,7 @@ public class BookActivity extends AppCompatActivity implements LoaderManager.Loa
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
 
-        empty_view = findViewById(R.id.empty_view);
+        emptyStateView = findViewById(R.id.empty_view);
 
         if(isConnected) {
             LoaderManager loaderManager = getLoaderManager();
@@ -33,8 +40,22 @@ public class BookActivity extends AppCompatActivity implements LoaderManager.Loa
         } else {
             loadingIndicator = findViewById(R.id.loading_indicator);
             loadingIndicator.setVisibility(View.GONE);
-            empty_view.setText(R.string.no_connection);
+            emptyStateView.setText(R.string.no_connection);
         }
+
+        ListView bookListView = (ListView) findViewById(R.id.list);
+        adapter = new BookAdapter(this, new ArrayList<Book>());
+        bookListView.setEmptyView(emptyStateView);
+        bookListView.setAdapter(adapter);
+        bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Book currentBook = adapter.getItem(i);
+                Uri bookUri = Uri.parse(currentBook.getBookUrl());
+                Intent bookPage = new Intent(Intent.ACTION_VIEW, bookUri);
+                startActivity(bookPage);
+            }
+        });
     }
 
     @Override
