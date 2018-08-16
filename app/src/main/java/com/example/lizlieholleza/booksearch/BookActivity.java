03cdbb87c37d60e1641c7354bbde612a16a612c6
Liz <lizlie.holleza@awsys-i.com>
2018-08-16 15:44:29 +0800

    check if connected to the internet and override the callbacks method of the LoaderManager

--- app/src/main/java/com/example/lizlieholleza/booksearch/BookActivity.java
+++ app/src/main/java/com/example/lizlieholleza/booksearch/BookActivity.java
@@ -1,13 +1,54 @@
 package com.example.lizlieholleza.booksearch;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class BookActivity extends AppCompatActivity {
+import java.util.List;
 
+public class BookActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>>{
+    private static final int LOADER_ID = 1;
+    private TextView empty_view;
+    private View loadingIndicator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        empty_view = findViewById(R.id.empty_view);
+
+        if(isConnected) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(LOADER_ID, null, this);
+        } else {
+            loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            empty_view.setText(R.string.no_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+
     }
 }
---
+++ app/src/main/java/com/example/lizlieholleza/booksearch/BookAdapter.java
@@ -0,0 +1,6 @@
+package com.example.lizlieholleza.booksearch;
+
+import android.widget.ArrayAdapter;
+
+public class BookAdapter extends ArrayAdapter<Book>{
+}
